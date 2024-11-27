bd04910aef4531214efa8d8cfce0468d6a225406
Hayden <hayden.dev.k@gmail.com>
2024-11-27 18:22:20 +0900

    Chore: sentry route

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const { body } = req;
 
   alert(body);
